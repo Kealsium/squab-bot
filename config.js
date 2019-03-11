e4c3b2da21b567a179bc2aaa9994a22108df3113
Kealsium <48448087+Kealsium@users.noreply.github.com>
2019-03-12 00:10:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    token: 'NTUyNDQ3MTAzNTAzNDMzNzQ5.D2fqXg.ipNIAcI-9Tx1yYS987eC5wbPBOs',
+    prefix: '!',
+    remind: '24h',
+    remindMessage: 'It is I, Squab bot! Im the attendance bot from Zephyrus guild, it looks like you havent told me if you are coming for the upcoming event! Pls go to the #guild-attendance channel to indicate whether you are coming or not pls uwu',
+    roles: {
+        guildies: '467056373151170590',
+        officer: '466883127118397450',
+        master: '466882965310537728'
+    },
+    channels: {
+        guildAttendance: '554658749613211649',
+        classAndGear: '554372300611256336'
+    },
+    successMessages: [
+        'Sure thing!',
+        'Whatever' ,
+		'Ah fineeeee' ,
+		'Its done!'
+    ],
+    classes: [
+        'Archer',
+        'Berserker', 
+        'Dark Knight', 
+        'Kunoichi', 
+        'Maehwa', 
+        'Musa', 
+        'Mystic', 
+        'Ninja', 
+        'Ranger', 
+        'Sorceress', 
+        'Striker', 
+        'Tamer', 
+        'Valkyrie', 
+        'Warrior', 
+        'Witch', 
+        'Wizard'
+    ]
+}
